2538f4fda8484fbca071d7ce8aa6239714565dce
Wohlever, Steve <wohlever@mitre.org>
2018-11-26 09:22:40 -0500

    adjusted indentations

--- webservice/src/main/java/org/mitre/synthea/webservice/RESTFulClient.java
+++ webservice/src/main/java/org/mitre/synthea/webservice/RESTFulClient.java
@@ -25,67 +25,67 @@ import org.json.JSONObject;
  */
 public class RESTFulClient {
 	public static void main(String[] args) {
-		
+
 		final String baseUrl = "http://localhost:8080/va-synthea/";
 		String uuid;
-		
-	    CloseableHttpClient client = HttpClients.createDefault();
-		CloseableHttpResponse response;
-		
-	    try {
-	    	
-	    	// Submit the request to generate CSV results
-			int population = 1000;
-		    String jsonRequest = "{\"population\": " + population + ",\"generateCSV\": true}";
 
-	    	HttpPost httpPost = new HttpPost(baseUrl + "/generate");
-		    httpPost.setEntity(new StringEntity(jsonRequest));
-		    httpPost.setHeader("accept", "application/json");
-		    httpPost.setHeader("content-type", "application/json");
-	    	response = client.execute(httpPost);
-		    if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-		    	JSONObject obj = new JSONObject(EntityUtils.toString(response.getEntity()));
-		    	uuid = obj.getString("uuid");
-		    	System.out.println("UUID: " + uuid);
-		    } else {
-		    	System.out.println("Submission of generation request failed");
-		    	return;
-		    }
-		    
-		    // Wait for and retrieve the results
-		    HttpGet httpGet = new HttpGet(baseUrl + "zip/" + uuid);
-	    	response = client.execute(httpGet);
-	    	int statusCode = response.getStatusLine().getStatusCode();
-		    while (statusCode != HttpStatus.SC_OK) { // GET succeeded
-		    	if (statusCode == HttpStatus.SC_ACCEPTED) { // Request is still in progress
-		    		
-		    		// Try again
-		    		response.close();		    		
-		    		System.out.println("Waiting for request to finish...");
-		    		Thread.sleep(10000); // 10 seconds
-		    		response = client.execute(httpGet);
-			    	statusCode = response.getStatusLine().getStatusCode();
-		    	} else { // There was an error of some kind
-		    		switch(statusCode) {
-		    			case HttpStatus.SC_BAD_REQUEST:
-		    				System.out.println("Could not retrieve results due to missing or malformed UUID");
-		    				break;
-		    			case HttpStatus.SC_NOT_FOUND:
-		    				System.out.println("Results not found for UUID " + uuid);
-		    				break;
-		    			case HttpStatus.SC_INTERNAL_SERVER_ERROR:
-		    				System.out.println("Could not retrieve results due to internal server error");
-		    				break;
-		    			default:
-		    				break;
-		    		}
-		    		
-		    		return;
-		    	}
-		    }
-		    
-		    // Save the results locally
-    		System.out.println("Saving results...");
+		CloseableHttpClient client = HttpClients.createDefault();
+		CloseableHttpResponse response;
+
+		try {
+
+			// Submit the request to generate CSV results
+			int population = 1000;
+			String jsonRequest = "{\"population\": " + population + ",\"generateCSV\": true}";
+
+			HttpPost httpPost = new HttpPost(baseUrl + "/generate");
+			httpPost.setEntity(new StringEntity(jsonRequest));
+			httpPost.setHeader("accept", "application/json");
+			httpPost.setHeader("content-type", "application/json");
+			response = client.execute(httpPost);
+			if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+				JSONObject obj = new JSONObject(EntityUtils.toString(response.getEntity()));
+				uuid = obj.getString("uuid");
+				System.out.println("UUID: " + uuid);
+			} else {
+				System.out.println("Submission of generation request failed");
+				return;
+			}
+
+			// Wait for and retrieve the results
+			HttpGet httpGet = new HttpGet(baseUrl + "zip/" + uuid);
+			response = client.execute(httpGet);
+			int statusCode = response.getStatusLine().getStatusCode();
+			while (statusCode != HttpStatus.SC_OK) { // GET succeeded
+				if (statusCode == HttpStatus.SC_ACCEPTED) { // Request is still in progress
+
+					// Try again
+					response.close();		    		
+					System.out.println("Waiting for request to finish...");
+					Thread.sleep(10000); // 10 seconds
+					response = client.execute(httpGet);
+					statusCode = response.getStatusLine().getStatusCode();
+				} else { // There was an error of some kind
+					switch(statusCode) {
+					case HttpStatus.SC_BAD_REQUEST:
+						System.out.println("Could not retrieve results due to missing or malformed UUID");
+						break;
+					case HttpStatus.SC_NOT_FOUND:
+						System.out.println("Results not found for UUID " + uuid);
+						break;
+					case HttpStatus.SC_INTERNAL_SERVER_ERROR:
+						System.out.println("Could not retrieve results due to internal server error");
+						break;
+					default:
+						break;
+					}
+
+					return;
+				}
+			}
+
+			// Save the results locally
+			System.out.println("Saving results...");
 			HttpEntity entity = response.getEntity();
 			if (entity != null) {
 				File zipfile = new File(uuid + ".zip");
