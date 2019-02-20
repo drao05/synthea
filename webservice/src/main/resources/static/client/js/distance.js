4efb9d934d7dc1870fba6c4feb25e315f3a812e8
kcrouch <kcrouch@mitre.org>
2019-02-20 12:19:12 -0500

    rough travel tracking

--- webservice/src/main/resources/static/client/js/distance.js
+++ webservice/src/main/resources/static/client/js/distance.js
@@ -1,3 +1,35 @@
+function generateBreadcrumbs(start = {latitude:0, longitude: 0}, end={latitude: 45, longitude: 45}, frequencyDistance=.25) {
+  const distance = calculateDistance(start.latitude, start.longitude, end.latitude, end.longitude);
+ 
+  
+}
+
+
+// Converts from degrees to radians.
+function toRadians(degrees) {
+  return degrees * Math.PI / 180;
+};
+ 
+// Converts from radians to degrees.
+function toDegrees(radians) {
+  return radians * 180 / Math.PI;
+}
+
+
+function bearing(lat1, lon1, lat2, lon2){
+  lat1 = toRadians(lat1);
+  lon1 = toRadians(lon1);
+  lat2 = toRadians(lat2);
+  lon2 = toRadians(lon2);
+
+  y = Math.sin(lon2 - lon1) * Math.cos(lat2);
+  x = Math.cos(lat1) * Math.sin(lat2) -
+        Math.sin(lat1) * Math.cos(lat2) * Math.cos(lon2 - lon1);
+  brng = Math.atan2(y, x);
+  brng = toDegrees(brng);
+  return (brng + 360) % 360;
+}
+
 //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 //:::                                                                         :::
 //:::  This routine calculates the distance between two points (given the     :::
@@ -26,7 +58,7 @@
 //:::                                                                         :::
 //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-function distance(lat1, lon1, lat2, lon2, unit) {
+function calculateDistance(lat1, lon1, lat2, lon2, unit) {
 	if ((lat1 == lat2) && (lon1 == lon2)) {
 		return 0;
 	}
