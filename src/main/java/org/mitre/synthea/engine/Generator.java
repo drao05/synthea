ee5ed91decdd5dc077c55aeb614a5d0400fe5c87
Wohlever, Steve <wohlever@mitre.org>
2019-04-03 15:55:02 -0400

    backed out incorrect key name change

--- src/main/java/org/mitre/synthea/engine/Generator.java
+++ src/main/java/org/mitre/synthea/engine/Generator.java
@@ -512,7 +512,7 @@ public class Generator {
     out.put(Person.SOCIOECONOMIC_CATEGORY, city.socioeconomicCategory(sesScore));
 
     if (this.onlyVeterans) {
-      out.put("veteran", Boolean.TRUE);
+      out.put("veteran_population_override", Boolean.TRUE);
     }
 
     int targetAge;
